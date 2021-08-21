fd2d04eacc31e8db168bef33db2e67fe5f508cb2
GABRIEL HAAB R. MACHADO GABRIEL HAAB RODRIGUES MACHADO <haabgabriel@gmail.com>
2021-08-21 19:27:51 -0300

    primeirocommit

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CoreModule } from './core/core.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [CoreModule, AppRoutingModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
